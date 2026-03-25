5204c2352673bca2b942c78ec183d1308f38b133
Leon Si <leon@leonsilicon.com>
2026-03-25 01:26:16 -0400

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,13 @@ import antfu from '@antfu/eslint-config';
 
 export default antfu(
 	{
+		type: 'app',
 		formatters: true,
 		react: true,
+		ignores: [
+			'**/target',
+			'**/src-tauri',
+		],
 		stylistic: {
 			indent: 'tab',
 		},
